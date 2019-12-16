10bffcd99d094ee218f990658e691f8358d7c1a6
senseyluiz <senseyluiz@gmail.com>
2019-12-16 13:54:32 -0200

    Atualização para responsivo

---
+++ projeto-03/js/scripts.js
@@ -0,0 +1,12 @@
+$(function(){
+		$('.menu-mobile').click(function(){
+		// O que vai acontecer quando clicar no menu-mobile
+		var listaMenu = $('.menu-mobile ul');
+
+		//Abrir ou fechar o menu após clicado no botão
+		listaMenu.slideToggle();
+		
+		
+	})
+
+})
