3df8b9d575e785edc6cf4d9f0733c5244243b15c
senseyluiz <senseyluiz@gmail.com>
2020-01-18 15:49:18 -0200

    Menu com scroll suave com Jquery

--- projeto-05-(Cozinhando com o Padeiro)/js/scripts.js
+++ projeto-05-(Cozinhando com o Padeiro)/js/scripts.js
@@ -26,3 +26,11 @@ $(function(){
         }
     })
 })
+
+// Criando scroll suave
+jQuery(document).ready(function($) { 
+    $(".scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);
+   });
+});
